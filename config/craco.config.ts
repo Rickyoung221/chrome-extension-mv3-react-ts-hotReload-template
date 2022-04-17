b6a024ee77b1ee89bb5931a417777965124ba10c
Weikeng Yang <weikengyang@gmail.com>
2022-04-17 05:01:02 -0700

    Remove rar, use craco. Config, tell the chrome about content and background.
    Rmove react-app-rewired instead, use craco.
    Edit the config, convert content script and background script to output file that matchs in manifest

---
+++ config/craco.config.ts
@@ -0,0 +1,31 @@
+const path = require('path');
+const rootPath = path.join(__dirname, '..');
+const srcPath = path.join(rootPath, 'src');
+const webpack = require('webpack');
+
+module.exports = {
+    webpack: {
+      configure: (webpackConfig, {env, paths}) => {
+        return {
+          ...webpackConfig,
+          entry: {
+            main: [env === 'development' &&
+            require.resolve('react-dev-utils/webpackHotDevClient'),paths.appIndexJs].filter(Boolean),
+            content: path.join(srcPath, 'content_script', 'index.tsx'),
+            background: path.join(srcPath, 'background_script', 'index.tsx'),
+            popup: path.join(srcPath, 'popup', 'index.tsx'),
+          },
+          output: {
+            ...webpackConfig.output,
+            filename: 'static/js/[name].js',
+          },
+          optimization: {
+            ...webpackConfig.optimization,
+            runtimeChunk: false,
+          }
+        }
+      },
+    }
+  }
+  
+export {}
